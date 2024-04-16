58d26bc1f260bb3cd1dafd5fd740b76c0700e8be
Daniel Rodrigues <danielrodriguescarvalho777@gmail.com>
2024-04-16 11:44:01 -0300

    learning operators

--- 5_operadores.rb
+++ 5_operadores.rb
@@ -12,7 +12,7 @@ mult = num1 * num2
 mod = num1 % num2
 exp = num1**num2
 
-puts sum,subt,div,mult,mod,exp
+puts sum, subt, div, mult, mod, exp
 
 # 2 - Operadores de Atribuição
 num1 += 1 # num1 = num1 + 1
@@ -24,9 +24,30 @@ num1 *= 1 # num1 = num1 * 1
 a = 5
 b = 4
 a, b = b, a
-puts a,b
+puts a, b
 
 # 4 - Operadores de Comparação
 bigger = num1 > num2
 smaller = num1 < num2
 equal = num1 == num2
+different = num1 !=  num2
+bigger_equal = num1 >= num2
+smaller_equal = num1 <= num2
+
+# 5 - Operadores lógicos
+puts((2>4) && (3>1))
+puts((2>4) || (3>1))
+
+# 6 - Operadores Especiais
+print (1..5).to_a
+print ('a'..'z').to_a
+
+# 7 - Precedência de Operadores
+puts 3+ 2*5
+puts (3 + 2)*5
+
+# 8 - Operador Binary Left Shift
+name = ""
+name << "Fulano "
+name << "Sicrano"
+puts name
