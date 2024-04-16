47872cc8e38c70bb6b72e3757817e9aa4fdf7380
Daniel Rodrigues <danielrodriguescarvalho777@gmail.com>
2024-04-16 12:09:23 -0300

    Exercise1 adding IMC category

--- Exercises/exercise1.rb
+++ Exercises/exercise1.rb
@@ -1,3 +1,12 @@
+puts "Categorias IMC"
+puts "< 16,5 - Peso muito abaixo do normal"
+puts "< 18,5 - Peso abaixo do normal"
+puts "18,5 - 24,99 = Peso normal"
+puts "25 - 29,99 = Pré-obeso"
+puts "30 - 34,99 = Obesidade Classe I"
+puts "35 - 39,99 = Obesidade Classe II"
+puts "> 40 = Obesidade Classe III"
+
 puts "Digite o seu peso"
 peso = gets.chomp.to_i
 
